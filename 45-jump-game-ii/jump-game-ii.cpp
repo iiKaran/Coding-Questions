78cd1ccc7d89ae05b62a22aa88b9612693f2b342
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-01-30 20:56:55 +0530

    Time: 218 ms (26.65%) | Memory: 20.6 MB (5.26%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int recall(vector<int>& nums,int ind,vector<int>& dp)
+    {
+        if(ind>=nums.size()-1)
+        return 0;
+        if(dp[ind]!=0)
+        return dp[ind];
+        int ans=10000;
+        for(int i=ind+1;i<=ind+nums[ind];i++)
+        {
+            int pick=1+recall(nums,i,dp);
+            ans=min(ans,pick);
+        }
+        return dp[ind]=ans;
+    }
+    int jump(vector<int>& nums) {
+        if(nums.size()==2)
+        return 1;
+        vector<int> dp(nums.size(),0);
+        int mans= recall(nums,0,dp);
+        return mans;
+        
+    }
+};
