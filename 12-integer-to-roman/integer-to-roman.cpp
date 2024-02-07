05727fd27f486f6d9e3bb8c263f8eaed80bd2eb6
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-02-07 21:45:56 +0530

    Time: 12 ms (25.10%) | Memory: 16.1 MB (5.04%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<string> thousands = {"", "M", "MM", "MMM"};
+        vector<string> hundreds = {"",  "C",  "CC",  "CCC",  "CD",
+                                   "D", "DC", "DCC", "DCCC", "CM"};
+        vector<string> tens = {"",  "X",  "XX",  "XXX",  "XL",
+                               "L", "LX", "LXX", "LXXX", "XC"};
+        vector<string> ones = {"",  "I",  "II",  "III",  "IV",
+                               "V", "VI", "VII", "VIII", "IX"};
+
+        string ans = "";
+        ans += thousands[num / 1000];
+        num = num % 1000;
+        cout << num << ",";
+        ans += hundreds[num / 100];
+        num = num % 100;
+        ans += tens[num / 10];
+        num = num % 10;
+        ans += ones[num % 10];
+        return ans;
+    }
+};
