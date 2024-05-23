03c38b6c6416e369748f51ea749f4a267b31f010
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-05-23 07:11:31 +0530

    Time: 9 ms (57.03%) | Memory: 18.4 MB (71.03%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,22 +1,61 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans ; 
-        map<int, int> freq; 
-         int n = nums.size(); 
-        for(int i =0; i < nums.size(); i++)
-        {
-            if(freq[nums[i]]>=n/3)
-            {
-                ans.push_back(nums[i]);
-                freq[nums[i]]= -1 ; 
-                continue; 
+        vector<int> ans ;
+         
+         int candidate1 , candidate2 ; 
+         candidate1= candidate2=INT_MIN ; 
+         int vote1,vote2 ; 
+         vote1= vote2 = 0 ; 
+
+         // 
+         for(auto element : nums){
+            if(element== candidate1){
+                vote1+=1 ; 
             }
-            if(freq[nums[i]]== -1 )
-            continue; 
-           
-           freq[nums[i]]++;
-        }
-        return ans ;
+            else if(vote1==0 && element != candidate2)
+            {
+                vote1+=1 ; 
+                candidate1= element ; 
+            }
+            
+           else if(element== candidate2){
+                vote2+=1 ; 
+            }
+           else if(vote2==0 && element!= candidate1)
+            {
+                vote2+=1 ; 
+                candidate2=element ; 
+            }
+            
+            else{
+               if(element!= candidate1)
+               vote1--; 
+                if(element != candidate2)
+               vote2--; 
+
+               
+            }
+
+         }
+         int cnt1 , cnt2;
+         cnt1=cnt2=0; 
+         for( auto elem : nums){
+            if(elem==candidate1){
+                cnt1++; 
+            }
+            if(elem==candidate2){
+                cnt2++; 
+            }
+         }
+         int n = nums.size(); 
+         if(cnt1> n/3){
+            ans.push_back(candidate1); 
+            }
+         if(cnt2>n/3){
+             ans.push_back(candidate2); 
+         }
+        sort(ans.begin(),ans.end());
+        return ans ; 
     }
 };
