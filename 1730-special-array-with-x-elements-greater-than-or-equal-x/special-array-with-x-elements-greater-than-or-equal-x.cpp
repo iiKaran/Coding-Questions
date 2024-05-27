d8f788bffb27af548d8cb9038860a6de0822e270
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-05-27 20:19:52 +0530

    Time: 2 ms (51.71%) | Memory: 10.5 MB (9.22%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,21 +1,42 @@
 class Solution {
+    int find_greater(int elem , int n, vector<int>&arr)
+    {
+        int first_ind= n ; 
+        int l , r ; 
+        l=0;  r= n-1;
+        int mid= (l+r)/2 ;
+        while(l<=r)
+        {
+            mid= (l+r)/2 ;
+            if(elem<=arr[mid])
+            {
+                first_ind= mid; 
+                r= mid-1 ; 
+            }
+            else{
+                l =mid+1;
+            }
+        }
+        return n - first_ind ; 
+    }
 public:
     int specialArray(vector<int>& nums) {
-        unordered_map<int,int> mp ; 
-        sort(nums.begin(), nums.end()); 
-        for( int x=0; x <= nums.size(); x++){
-            for( int i =0; i  < nums.size(); i++)
-             if(nums[i]>=x)
-             {
-                mp[x] += nums.size()-i ;
-                break; 
-             }
-        }
-        for(auto it: mp)
-        {
-            if(it.first == it.second)
-            return it.first ;
-        }
-        return -1 ; 
+       int n = nums.size(); 
+       vector<int> frequency(n+1,0); 
+
+       for( int num:nums){
+            frequency[min(num,n)]++; 
+            // ans lies in the 1 to n only 
+       }
+       // now check equality 
+       
+       int curr_greater = 0 ; 
+       for(int ans= n; ans>0; ans--){
+            curr_greater += frequency[ans]; 
+            if(ans== curr_greater)
+            return ans ; 
+       }
+       return -1 ; 
+
     }
 };
