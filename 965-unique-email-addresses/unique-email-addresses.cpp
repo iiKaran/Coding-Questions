2415559226e1c1f79f9a35d9204b41f95704e651
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-05-29 06:25:14 +0530

    Time: 32 ms (11.45%) | Memory: 19.5 MB (19.29%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.cpp
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
-    unordered_map<string,int> mp ;
+  map<string,int> mp ;
     int cnt=0; 
     for(auto text: emails){
       string atTheRate = "@";
