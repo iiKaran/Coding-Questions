11e3da92c0de17c17e460a635382ff53a5a9dd5e
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-05-29 06:22:50 +0530

    Time: 36 ms (8.29%) | Memory: 19.5 MB (19.29%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+    unordered_map<string,int> mp ;
+    int cnt=0; 
+    for(auto text: emails){
+      string atTheRate = "@";
+    // Find the position of the atTheRate
+    int position = text.find(atTheRate);
+    if (position != string::npos) {
+        //  found then extract
+        string name = text.substr(0,position);
+        string domain = text.substr(position);
+        
+        // work on the name
+        int dotPos = name.find("."); 
+        while(dotPos != string::npos){
+            name = name.erase(dotPos,1);
+            dotPos = name.find(".");
+        }
+        // remove all the dots
+        // ignore string after add 
+        int plusPos = name.find("+"); 
+        if(plusPos != string::npos){
+            string modified = name.substr(0,plusPos);
+            name= modified;
+        }
+        string final = name+domain; 
+        cout<<final<<endl;
+        mp[final]+=1 ; 
+    }
+    }
+    
+    for(auto i : mp){
+
+         
+            cnt+=1 ; 
+         
+    }
+    return cnt ;
+    }
+};
