161b1cbbefb4a071141a8b94717daad40ac7818c
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-06-12 14:16:16 +0530

    Time: 55 ms (54.27%) | Memory: 34 MB (72.24%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -5,8 +5,6 @@ public:
         int n = mat[0].size();
         vector<vector<int>> ans(m, vector<int>(n, INT_MAX));
         queue<pair<int, int>> q;
-
-        // Initialize the queue with all the cells that contain 0 and set their distance to 0
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (mat[i][j] == 0) {
@@ -15,11 +13,8 @@ public:
                 }
             }
         }
-
-        // Directions array to move in 4 directions: up, down, left, right
         vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        // Process the queue using BFS
         while (!q.empty()) {
             auto [i, j] = q.front();
             q.pop();
