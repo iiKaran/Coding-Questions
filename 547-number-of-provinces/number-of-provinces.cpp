88053651025b6ba39129db23f3e56c2357eaab9f
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-06-13 10:15:38 +0530

    Time: 17 ms (85.09%) | Memory: 18 MB (73.94%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,33 +1,23 @@
 class Solution {
-    void dfs( int node , vector<bool> & vis, unordered_map<int , vector<int> >  & adj){
+    void dfs( int node , vector<bool> & vis, vector<vector<int>>& mat){
         
         vis[node]= 1 ; 
-        for( auto  i : adj[node]){
-            if(vis[i]==false){
-                dfs(i,vis,adj);
+        for( int col =0; col< mat[node].size(); col++){
+            if(mat[node][col]== 1 && vis[col]== false){
+                dfs(col , vis , mat);
             }
         }
     }
 public:
     int findCircleNum(vector<vector<int>>& mat) {
-        unordered_map<int , vector<int> > adj ; 
-        for( int i =0; i < mat.size(); i++ ){
-            for( int j =0; j < mat[0].size(); j++){
-
-                if(mat[i][j]==1){
-                adj[i].push_back(j); 
-                adj[j].push_back(i);
-                }
-            }
-        }
-
+       
         int ans =0; 
         vector<bool> visited(mat.size()+1, false);
         for( int i=0; i < mat.size();i++){
 
                 if(!visited[i]){
                     ans+=1 ; 
-                    dfs(i, visited, adj); 
+                    dfs(i, visited, mat); 
                 }
         }
         return ans ;
