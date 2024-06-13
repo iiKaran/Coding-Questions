2fec5e49c4e1ec69db617e341f2745a6b927d852
Karan Sehgal <104912927+iiKaran@users.noreply.github.com>
2024-06-13 18:38:34 +0530

    Time: 10 ms (60.15%) | Memory: 13.6 MB (33.10%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<string> v(numRows, "");
-        int i =0;
-        int n = s.length();
-        while(i<n){
-            //for up to down traverse
-            for(int j =0; j<numRows && i<n; j++ ){
-                v[j].push_back(s[i++]);
+        vector<string> vec(numRows ,"");
+        int n = s.length(); 
+        int i =0; 
+        while(i< n){
+            for(int j =0; j < numRows && i < n ; j++){
+                vec[j].push_back(s[i++]);
             }
-            //down to up traverse
-            for(int j =numRows-2; j>=1 && i<n; j--){
-                v[j].push_back(s[i++]);
+            for( int j = numRows-2; j>=1 && i < n; j--){
+                vec[j].push_back(s[i++]);
             }
         }
-        //store row wise
-        string res ="";
-        for(auto i : v){
-            res+=i;
+        string ans ="";
+        for(auto i : vec){
+            ans+=i; 
         }
-        return res;
+        return ans;
     }
 };
